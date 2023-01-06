1833152d13606d2f9960d0a9497b66b2b7277403
ClickRPG <121915202+ClickRPG@users.noreply.github.com>
2023-01-05 20:01:24 -0800

    Added smooth numbers.

--- app.js
+++ app.js
@@ -1,95 +1,98 @@
 $(document).ready(function () {
     var logs = 0;
-    var money = 0;
+    var money = 10000;
     var logPlus = 1;
     var autoLogPlus = 0;
     var autoChopperPrice = 100;
     var logPrice = 1;
     var menu;
-    setInterval(function(){
+    setInterval(function () {
         logs += autoLogPlus;
         changeInventory();
         changeMarket();
-    }, 1000 );
+    }, 1000 / 60);
 
-    $("#chop").click(function(){
+    $("#chop").click(function () {
         logs += logPlus;
         changeInventory();
         changeMarket();
     });
 
-    $("#autoChopper").click(function(){
+    $("#autoChopper").click(function () {
         money -= autoChopperPrice;
-        autoLogPlus++;
+        autoLogPlus += 0.01;
         changeInventory();
         changeMarket();
     });
 
-    $("#sell1").click(function(){
+    $("#sell1").click(function () {
         logs--;
         money += logPrice;
         changeInventory();
         changeMarket();
     });
 
-    $("#sell10").click(function(){
-        logs-=10;
+    $("#sell10").click(function () {
+        logs -= 10;
         money += logPrice * 10;
         changeInventory();
         changeMarket();
     });
 
-    $("#sellAll").click(function(){
+    $("#sellAll").click(function () {
         money += logPrice * logs;
         logs = 0;
         changeInventory();
         changeMarket();
     });
 
-    $("#visit").click(function(){
+    $("#visit").click(function () {
         menu = switchMenu("marketplace");
         changeMarket();
     });
 
-    $("#return").click(function(){
-        menu =switchMenu("main")
+    $("#return").click(function () {
+        menu = switchMenu("main")
     });
 
+
     function changeInventory() {
+
+        
         $("#money").html("Money: $" + money);
 
         if (logs == 1) {
-            $("#logs").html("You now own " + logs + " log.")
+            $("#logs").html("You now own " + logs.toFixed(1) + " log.")
         } else {
-            $("#logs").html("You now own " + logs + " logs.")
+            $("#logs").html("You now own " + logs.toFixed(1) + " logs.")
         }
     }
 
-    function changeMarket(){
-        if(logs > 0){
+    function changeMarket() {
+        if (logs > 0) {
             $("#sellAll").css("display", "block")
-        }else{
+        } else {
             $("#sellAll").css("display", "none")
         }
-        if(logs >= 1){
+        if (logs >= 1) {
             $("#sell1").css("display", "block")
-        }else{
+        } else {
             $("#sell1").css("display", "none")
         }
-        if(logs >= 10){
+        if (logs >= 10) {
             $("#sell10").css("display", "block")
-        }else{
+        } else {
             $("#sell10").css("display", "none")
         }
 
-        if (money >= autoChopperPrice){
+        if (money >= autoChopperPrice) {
             $("#autoChopper").css("display", "block");
-        }else{
+        } else {
             $("#autoChopper").css("display", "none");
         }
     }
 
-    function switchMenu(menu){
+    function switchMenu(menu) {
         $(".menus").children().css("display", "none");
         $("." + menu).css("display", "block");
         return menu;
